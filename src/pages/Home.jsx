bbe9cd639d84d9ce9101d8e80275902f1d1a0bae
Seun Fashina <fashinaoluwaseun36@gmail.com>
2023-03-15 14:32:05 -0700

    made the Cta component reusable

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,23 @@
-import BannerSlide from "../components/BannerSlide"
+import BannerSlide from "../components/BannerSlide";
 import Category from "../components/Category";
 import Cta from "../components/Cta";
 import Featured from "../components/Featured";
 
-const Home = () => {
-    return (
-      <>
-        <BannerSlide />
-        <Category />
-        <Featured/>
-        <Cta/>
-      </>
-    );
-}
 
-export default Home
+const Home = () => {
+
+  return (
+    <>
+      <BannerSlide />
+      <Category />
+      <Featured />
+      <Cta
+        actionText="Ace your programming skill today!"
+        buttonText="Go to library"
+        url="library/#"
+      />
+    </>
+  );
+};
+
+export default Home;
