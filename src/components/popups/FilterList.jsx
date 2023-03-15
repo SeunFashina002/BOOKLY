eafaa7b437dfec8fc9910931e8a4c6cf0337e134
Seun Fashina <fashinaoluwaseun36@gmail.com>
2023-03-15 14:28:35 -0700

    worked on the filter by component

--- src/components/popups/FilterList.jsx
+++ src/components/popups/FilterList.jsx
@@ -4,12 +4,15 @@ export const FilterList = ({ isVisible }) => {
 
     
   return (
-    <div style={{display: display}} className="bg-veryLightGray w-full md:w-1/2 rounded-md p-3 border-lightBrown border mb-6">
-      <h4 className="font-bold font-inter text-md uppercase">Filter By</h4>
+    <div
+      style={{ display: display }}
+      className="dark:bg-thinDark bg-veryLightGray w-full md:w-1/2 rounded-md p-3 border-lightBrown border mb-6"
+    >
+      <h4 className="dark:text-veryLightGray font-bold font-inter text-md uppercase">Filter By</h4>
       <hr />
       <div className="my-4">
         <h2 className="font-bold font-sans text-md">Category</h2>
-        <ul className="text-sm font-poppins">
+        <ul className="text-sm font-poppins dark:text-veryLightGray">
           <li>Data Science</li>
           <li>Mobile Development</li>
           <li>Web Development</li>
@@ -20,7 +23,7 @@ export const FilterList = ({ isVisible }) => {
       <hr />
       <div className="my-4">
         <h2 className="font-bold font-sans text-md">Some other stuff</h2>
-        <ul className="text-sm font-poppins">
+        <ul className="text-sm font-poppins dark:text-veryLightGray">
           <li>Item goes here</li>
           <li>Item goes here</li>
         </ul>
