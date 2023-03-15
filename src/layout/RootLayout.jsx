b54289fc2a2d064332174607f8613ba5f7cbb1dc
Seun Fashina <fashinaoluwaseun36@gmail.com>
2023-03-15 14:31:05 -0700

    added dark class to change the theme behaviour

--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -1,4 +1,4 @@
-import { NavLink, Outlet} from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 import { NavHashLink } from "react-router-hash-link";
 import Footer from "../components/Footer";
 import { ThemeIcon } from "../components/icons/ThemeIcon";
@@ -7,7 +7,6 @@ import { MobileMenu } from "../components/popups/MobileMenu";
 import { useState } from "react";
 
 const RootLayout = () => {
-
   let [icon, setIcon] = useState(true);
 
   const changeIcon = () => {
@@ -16,15 +15,17 @@ const RootLayout = () => {
 
   return (
     <>
-      <nav className="z-20 sm:px-2 md:px-12  py-3 bg-veryLightGray border-b-2 border-lightBrown sticky top-0 z-20">
+      <nav className="dark:bg-darkMoon z-20 sm:px-2 md:px-12  py-3 bg-veryLightGray border-b-2 border-lightBrown dark:border-b-0 sticky top-0 z-20">
         <div className="relative  flex flex-row justify-between items-center text-navLinkBlack w-full">
           <h1 className="text-2xl font-poppins flex flex items-center">
-            <span className="mx-1  flex flex items-center">
+            <span className="mx-1  flex flex items-center dark:text-white">
               <ion-icon name="book-outline"></ion-icon>
             </span>
-            <NavLink to="/">BOOKLY</NavLink>
+            <NavLink className="dark:text-lightGray" to="/">
+              BOOKLY
+            </NavLink>
           </h1>
-          <ul className="hidden  items-center font-sans text-lg md:text-sm sm:absolute  sm:drop-shadow-2xl sm:rounded-lg sm:bg-veryLightGray justify-center sm:top-24 sm:h-72 sm:w-full md:h-0 md:top-0 md:rounded-0 md:drop-shadow-none md:p-0 md:flex md:flex-row md:space-x-12 md:relative">
+          <ul className="hidden dark:text-lightGray items-center font-sans text-lg md:text-sm sm:absolute  sm:drop-shadow-2xl sm:rounded-lg sm:bg-veryLightGray justify-center sm:top-24 sm:h-72 sm:w-full md:h-0 md:top-0 md:rounded-0 md:drop-shadow-none md:p-0 md:flex md:flex-row md:space-x-12 md:relative">
             <li className="hover:text-lightBrown">
               <NavLink to="/library">Library</NavLink>
             </li>
