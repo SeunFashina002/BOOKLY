7e07048980982e9c0e0522d8f6dd293cf084b5fb
Seun Fashina <fashinaoluwaseun36@gmail.com>
2023-03-15 14:09:26 -0700

    added darkmode functionality for system and user choice

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,34 @@
-import { createBrowserRouter, Route, createRoutesFromElements, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  Route,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
 import RootLayout from "./layout/RootLayout";
-import './App.css'
+import "./App.css";
 import Home from "./pages/Home";
 import { Library } from "./pages/Library";
+import { ScrollToTop } from "react-router-scroll-to-top";
 
-  const router = createBrowserRouter(
-    createRoutesFromElements(
-      <Route path="/" element={<RootLayout />}>
-        <Route index element={<Home />} />
-        <Route index element={<Home />} />
-        <Route path="library" element={<Library />} />
-      </Route>
-    )
-  );
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route
+      path="/"
+      element={
+        <ScrollToTop>
+          <RootLayout />
+        </ScrollToTop>
+      }
+    >
+      <Route index element={<Home />} />
+      <Route path="library" element={<Library />} />
+    </Route>
+  )
+);
 
 function App() {
-
-  return (
-    <RouterProvider router={router}/>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
