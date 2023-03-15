cb8277f134e53e2d35b77fbcb5cdcd6b23e786e9
Seun Fashina <fashinaoluwaseun36@gmail.com>
2023-03-15 14:25:59 -0700

    worked on the switch theme mode functionality

--- src/components/icons/ThemeIcon.jsx
+++ src/components/icons/ThemeIcon.jsx
@@ -1,14 +1,43 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export const ThemeIcon = () => {
 
-    let [icon, setIcon] = useState(false)
+  let [icon, setIcon] = useState(false)
+  let [theme, setTheme] = useState(null);
 
-    const changeIcon = () => setIcon((prevState) => !prevState);
-    let displayIcon = icon ? "sunny-outline" : "moon-outline";
-    return (
-      <button className="bg-lightGray hover:text-brown hover:bg-darkMoon flex flex items-center justify-center rounded-full font-bold text-darkMoon hover:text-lightBrown py-1 px-2 text-2xl" onClick={changeIcon}>
-        <ion-icon name={displayIcon} className="font-bold"></ion-icon>
-      </button>
-    );
+  useEffect(() => {
+    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      setTheme('dark')
+    } else {
+      setTheme('light')
+    }
+  }, [])
+
+  
+
+
+  useEffect(() => {
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark")
+    } else {
+      document.documentElement.classList.remove("dark")
+    }
+  }, [theme])
+
+  const handleThemeSwitch = () => {
+    setTheme(theme === "dark" ? "light" : "dark")
+    
+  }
+
+  const changeIcon = () => {
+    setIcon((prevState) => !prevState);
+    handleThemeSwitch()
+  };
+
+  let displayIcon = icon ? "moon-outline" : "sunny-outline";
+  return (
+    <button className="bg-lightGray dark:bg-darkMoon dark:text-brown hover:text-brown hover:bg-darkMoon flex flex items-center justify-center rounded-full font-bold text-darkMoon hover:text-lightBrown py-1 px-2 text-2xl" onClick={changeIcon}>
+      <ion-icon name={displayIcon} className="font-bold"></ion-icon>
+    </button>
+  );
 }
